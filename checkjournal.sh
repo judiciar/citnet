0f446f0e3dd30926229eb3d9946d82258915c6cb
cagalati <doc@cagl.ro>
2020-04-16 18:03:45 +0300

    First commit

--- checkjournal.sh
+++ checkjournal.sh
@@ -7,8 +7,8 @@ else
 fi
 echo "Se verifica fisierele prelucrate de CitNet dupa criteriul: since $timestamp"
 echo "Nu au regex de document si sunt etichetate ca citatii (probabil ok):"
-journalctl -u citnetd --since "$timestamp" | grep citatii | grep -P -v ".*[0-9]+-(?!1([0-2])-)[1-9][0-9]*-." | sort -t"]" -u -k2,2
+journalctl -u citnetd --since "$timestamp" | grep "citatii " | grep -P -v ".*[0-9]+-(?!1([0-2])-)[1-9][0-9]*-." | sort -t"]" -u -k2,2
 echo -e "\nNu au regex de document si sunt etichetate ca documente (de verificat):"
-journalctl -u citnetd --since "$timestamp" | grep documente | grep -P -v ".*[0-9]+-(?!1([0-2])-)[1-9][0-9]*-." | sort -t"]" -u -k2,2
+journalctl -u citnetd --since "$timestamp" | grep "documente " | grep -P -v ".*[0-9]+-(?!1([0-2])-)[1-9][0-9]*-." | sort -t"]" -u -k2,2
 echo -e "\nAu regex de document si sunt etichetate ca citatii (posibile erori ale CitNet):"
-journalctl -u citnetd --since "$timestamp" | grep citatii | grep -P ".*[0-9]+-(?!1([0-2])-)[1-9][0-9]*-." | sort -t"]" -u -k2,2
+journalctl -u citnetd --since "$timestamp" | grep "citatii " | grep -P ".*[0-9]+-(?!1([0-2])-)[1-9][0-9]*-." | sort -t"]" -u -k2,2
